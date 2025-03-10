2811526f3bd746fd98c20ee24c35e4ea4d8c6611
Marietta Racancoj <s1064730@monmouth.edu>
2025-03-10 18:28:31 -0400

    HashRouter and homepage added to package.json

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { HashRouter as Router } from "react-router-dom";  //possible fix for white page
 
 //https://www.youtube.com/watch?v=FHn8c4Rk_yo
 
@@ -11,4 +12,8 @@ root.render(
     <App />
   // </React.StrictMode>
 );
+
+
+
+
 
