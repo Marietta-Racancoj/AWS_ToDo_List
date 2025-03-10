f2a503a87ad5d28d08f86ba56258fe5feb3ccf1b
Marietta Racancoj <s1064730@monmouth.edu>
2025-03-10 18:34:01 -0400

    deleted

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter as Router } from "react-router-dom";  //possible fix for white page
+//import { HashRouter as Router } from "react-router-dom";  //possible fix for white page
 
 //https://www.youtube.com/watch?v=FHn8c4Rk_yo
 
