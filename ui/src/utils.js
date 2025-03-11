9c11007b440d570c37e98a7e2a364d9ad44c721b
Marietta-Racancoj <144953735+Marietta-Racancoj@users.noreply.github.com>
2025-03-11 11:43:34 -0400

    Update utils.js with task API_URL path

--- ui/src/utils.js
+++ ui/src/utils.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://j3mvz5yfwdbuaeqd4u6u4akaqa0uapct.lambda-url.us-east-1.on.aws/";
+export const API_URL = "https://j3mvz5yfwdbuaeqd4u6u4akaqa0uapct.lambda-url.us-east-1.on.aws/task";
 
 //"http://localhost:3301/task";
 
